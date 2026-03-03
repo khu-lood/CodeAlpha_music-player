59cc9491e6be536e03050bebfe781b0e6d01906a
khulood_ikramullah <kood90979@gmail.com>
2026-03-03 16:00:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const songs = [
     {
-        name: "song1",
-        title: "hamud habibi",
-        artist: "Soft Artist",
-        cover: "images/cover1.jpg"
-    },
-    {
         name: "song2",
         title: "tab tabi tab",
         artist: "Chill Mood",
         cover: "images/cover2.jpg"
+    },
+    {
+        
+        name: "song1",
+        title: "hamud habibi",
+        artist: "Soft Artist",
+        cover: "images/cover1.jpg"
+        
     },
     {
         name: "song3",
